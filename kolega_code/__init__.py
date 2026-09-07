15c6871e9729456db1d9928e5fbc57184e9304a5
evandempsey <evandempsey@gmail.com>
2026-09-07 09:54:17 +0100

    chore: release v0.36.0

--- kolega_code/__init__.py
+++ kolega_code/__init__.py
@@ -174,4 +174,4 @@ __all__ = [
     "parse_git_status_output",
 ]
 
-__version__ = "0.35.1"
+__version__ = "0.36.0"
--- kolega_code/agent/__init__.py
+++ kolega_code/agent/__init__.py
@@ -51,4 +51,4 @@ __all__ = [
     "ToolExtension",
 ]
 
-__version__ = "0.35.1"
+__version__ = "0.36.0"
